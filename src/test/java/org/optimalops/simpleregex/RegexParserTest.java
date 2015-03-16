8ee12423fa9294eaddd01b68702bdf8ca8dc29fe
Gareth Bowles <gareth@optimalops.net>
2015-03-16 09:21:07 -0700

    More tests

--- src/test/java/org/optimalops/simpleregex/RegexParserTest.java
+++ src/test/java/org/optimalops/simpleregex/RegexParserTest.java
@@ -30,6 +30,26 @@ public class RegexParserTest {
     }
     
     @Test
+    public void testExtraMiddleStarMatch() throws Exception {
+        assertTrue(RegexParser.match("abcd*efgh", "abcdefgh"));
+    }
+
+    @Test
+    public void testMiddleStarSubstituteMatch() throws Exception {
+        assertTrue(RegexParser.match("ab*fgh", "abcdefgh"));
+    }
+
+    @Test
+    public void testExtraEndStarMatch() throws Exception {
+        assertTrue(RegexParser.match("abcdefgh*", "abcdefgh"));
+    }
+
+    @Test
+    public void testEndStarSubstituteMatch() throws Exception {
+        assertTrue(RegexParser.match("ab*", "abcdefgh"));
+    }
+
+    @Test
     public void testInitialPlusMatch() throws Exception {
         assertTrue(RegexParser.match("+defgh", "abcdefgh"));
     }
@@ -37,6 +57,26 @@ public class RegexParserTest {
     @Test
     public void testInitialPlusMismatch() throws Exception {
         assertFalse(RegexParser.match("+abcdefgh", "abcdefgh"));
+    }
+
+    @Test
+    public void testMiddlePlusMatch() throws Exception {
+        assertTrue(RegexParser.match("ab+gh", "abcdefgh"));
+    }
+
+    @Test
+    public void testMiddlePlusMismatch() throws Exception {
+        assertFalse(RegexParser.match("abcd+efgh", "abcdefgh"));
+    }
+
+    @Test
+    public void testEndPlusMatch() throws Exception {
+        assertTrue(RegexParser.match("abcdefg+", "abcdefgh"));
+    }
+
+    @Test
+    public void testEndPlusMismatch() throws Exception {
+        assertFalse(RegexParser.match("abcdefgh+", "abcdefgh"));
     }
 
     @Test
